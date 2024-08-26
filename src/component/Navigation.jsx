db922dd7b984718cd9a4ce4bd571a2bc075af53a
vishnu <vishnu@dieofficial.com>
2024-08-26 22:33:44 +0400

    navigation text added

--- src/component/Navigation.jsx
+++ src/component/Navigation.jsx
@@ -6,6 +6,9 @@ const Navigation = () => {
     <nav className="absolute top-0 w-full px-[70px] h-[90px] flex items-center">
       <div className="w-full flex justify-between gap-5">
         <TransitionLink href="/" label="vishnu" />
+        <p className="cursor-pointer text-sm font-thin uppercase">
+          Current creative developer @ richylife club
+        </p>
         <div className="bg-black text-white px-5 py-2 text-xs ">
           <LiveClock />
         </div>
