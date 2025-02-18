83d7df10fc58ddc94d6209cab0882644a3b776a3
vishnu <vishnu@dieofficial.com>
2025-02-18 17:49:15 +0400

    added icon library and hero section arrow

--- src/component/home/HeroSection.jsx
+++ src/component/home/HeroSection.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import TextMask from "../textmask/TextMask";
 import StickyRounds from "../animations/StickyRounds";
 import ScrollAnimationText from "../scrollAnimationText/scrollAnimationText";
+import { MoveRight, MoveDown } from "lucide-react";
 
 function HeroSection() {
   return (
     <div className="h-screen relative">
       <div className="absolute top-[30%] md:top-1/3 px-[20px] md:px-[70px] font-thin text-sm">
-        <span className="hidden md:block">Hover </span>
-        <span className="block md:hidden">Scroll</span>
+        <span className="hidden md:flex justify-center items-center">
+          Hover
+          <MoveRight />
+        </span>
+
+        <span className="flex md:hidden justify-center items-center">
+          <MoveDown />
+          Scroll
+        </span>
       </div>
       <TextMask />
       <ScrollAnimationText />
